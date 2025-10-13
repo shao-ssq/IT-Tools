f1a87fc9bab0297f87f1690776083028b32035b7
燕双嘤 <1966799809@qq.com>
2025-10-13 15:35:56 +0800

    修改悬停颜色

--- src/components/ToolCard.vue
+++ src/components/ToolCard.vue
@@ -10,7 +10,7 @@ const theme = useThemeVars();
 
 <template>
   <router-link :to="tool.path" class="decoration-none">
-    <c-card class="h-full transition transition-duration-0.5s !border-2px !hover:border-primary">
+    <c-card class="h-full transition transition-duration-0.5s !border-2px !hover:border-blue">
       <div flex items-center justify-between>
         <n-icon class="text-neutral-400 dark:text-neutral-600" size="40" :component="tool.icon" />
 
