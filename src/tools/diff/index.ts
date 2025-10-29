1d57b99b9f8daaaf819624b75e0af5239a44781f
燕双嘤 <1966799809@qq.com>
2025-10-29 20:17:36 +0800

    新增：文本对比

--- src/tools/diff/index.ts
+++ src/tools/diff/index.ts
@@ -1,4 +1,4 @@
-import { Braces } from '@vicons/tabler';
+import { FileDiff } from '@vicons/tabler';
 import { defineTool } from '../tool';
 
 export const diffTools = defineTool({
@@ -7,6 +7,6 @@ export const diffTools = defineTool({
   description: 'GitHub样式的文本对比模式，更加适合程序员',
   keywords: ['json', 'xml', '可视化'],
   component: () => import('./google.vue'),
-  icon: Braces,
+  icon: FileDiff,
   createdAt: new Date('2025-10-29'),
 });
